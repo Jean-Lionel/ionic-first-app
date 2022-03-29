852ba3c9eee5d57082cf8860b241393a5b8cffda
Jean-Lionel <nijeanlionel@gmail.com>
2022-03-29 11:07:39 +0200

    display ionic list

---
+++ src/pages/MemoriesPage.vue
@@ -0,0 +1,37 @@
+<template>
+    <ion-page>
+        <ion-header>
+            <ion-toolbar>
+                <ion-title>
+                Je suis en bonne santé
+                 </ion-title>
+            </ion-toolbar>
+            
+        </ion-header>
+        <ion-content>
+            <ion-list>
+                <ion-item>Lionel</ion-item>
+                <ion-item>Lionel 2</ion-item>
+                <ion-item>Lionel 3</ion-item>
+            </ion-list>
+        </ion-content>
+
+    </ion-page>
+</template>
+
+<script>
+import {IonPage,IonHeader,IonTitle, IonContent, IonList, IonItem, IonToolbar} from '@ionic/vue';
+
+export default {
+    components:{
+        IonPage,
+        IonHeader,
+        IonTitle,
+        IonContent,
+        IonList,
+        IonItem,
+        IonToolbar
+    }
+    
+}
+</script>
