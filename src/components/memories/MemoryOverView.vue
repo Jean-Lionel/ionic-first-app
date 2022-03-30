f5a300beb7d814c4b94a488738899ef5129e9d39
Jean-Lionel <nijeanlionel@gmail.com>
2022-03-30 05:23:11 +0200

    create over view component

---
+++ src/components/memories/MemoryOverView.vue
@@ -0,0 +1,21 @@
+<template>
+  <ion-img :src="memory.image" :alt="memory.title"></ion-img>
+  <h2>{{memory.title}}</h2>
+  <p>{{memory.body}}</p>
+</template>
+
+<script>
+import {IonImg} from "@ionic/vue";
+
+export default {
+    props: ["memory"],
+    components:{
+        IonImg
+    }
+
+}
+</script>
+
+<style>
+
+</style>
--- src/pages/DetailPage.vue
+++ src/pages/DetailPage.vue
@@ -1,24 +1,8 @@
 <template>
     <ion-page>
-        <base-layout :page-title="loadMemory ? loadMemory.title : 'loiding'" default-back-link="/">   
-        <ion-card v-if="loadMemory">
-        <ion-img :src="loadMemory.image"></ion-img>
-        <ion-card-content>
-            <ion-card-header>
-            <ion-card-subtitle>{{loadMemory.id}}</ion-card-subtitle>
-            <ion-card-title>{{loadMemory.title}}</ion-card-title>
-            </ion-card-header>
-            <p>
-                {{loadMemory.body}}
-            </p>
-            <ion-item>
-            <ion-button fill="solid">Action</ion-button>
-            <ion-icon name="heart" slot="end"></ion-icon>
-            <ion-icon name="share" slot="end"></ion-icon>
-            </ion-item>
-        </ion-card-content>
-        </ion-card>
-        <h1 v-else>Could not Be loadind</h1>
+        <base-layout :page-title="loadMemory ? loadMemory.title : 'loiding'" default-back-link="/">
+        <h1 v-if="!loadMemory">Could not Be loadind</h1>
+        <memory-over-view v-else  :memory="loadMemory"></memory-over-view>
 
         </base-layout>
     </ion-page>
@@ -27,29 +11,15 @@
 <script>
 import {
     IonPage,
-    IonCard,
-    IonImg,
-    IonCardContent,
-    IonCardHeader,
-    IonCardTitle,
-    IonCardSubtitle,
-    IonButton,
-    IonItem,
-    IonIcon,
+  
     } from '@ionic/vue'
+import MemoryOverView from '../components/memories/MemoryOverView.vue';
 
 export default {
     components:{
         IonPage,
-        IonCard,
-        IonImg,
-        IonItem,
-        IonCardHeader,
-        IonCardContent,
-        IonCardTitle,
-        IonCardSubtitle,
-        IonButton,
-        IonIcon
+        MemoryOverView,
+
     },
     data(){
         return {
