347d346086f837b10ffff1cdf17d64a0dfe0beec
Jean-Lionel <nijeanlionel@gmail.com>
2022-03-30 04:31:01 +0200

    it good work

--- src/components/memories/MemoryListeItem.vue
+++ src/components/memories/MemoryListeItem.vue
@@ -1,10 +1,21 @@
 <template>
     <ion-page>
+        <ion-item  :router-link="`/memories/${memory.id}`" :key="memory.id">         
+            <ion-thumbnail slot="start">
+            <ion-img :src="memory.image" :alt="memory.title"></ion-img>
+            </ion-thumbnail>
+            <ion-label> {{memory.title}}</ion-label>
+         </ion-item>
     </ion-page>
 </template>
 
 <script>
+import {IonPage, IonItem, IonThumbnail, IonImg,IonLabel} from "@ionic/vue"
 export default {
+    props: ["memory"],
+    components:{
+        IonPage, IonItem, IonThumbnail, IonImg,IonLabel
+    }
     
 }
 </script>
